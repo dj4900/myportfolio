8ad4ee7406f0f672c6651bff62d42acb93fad71a
dj4900 <daniel.jacobs@gmx.ch>
2024-12-14 09:30:40 +0100

    Re-added: About Me Section

--- Script/script-index.js
+++ Script/script-index.js
@@ -103,3 +103,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// Scroll-Animation
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".fade-in, .skills-meter");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in-visible");
+            }
+        });
+    });
+
+    elements.forEach((el) => observer.observe(el));
+});
